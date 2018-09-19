de1fa090c140b6bd76fda25a34abf320a5eec83f
Dustin Tran <trandustin@google.com>
2018-09-19 10:15:47 -0700

    Initial commit
    PiperOrigin-RevId: 213647914

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mesh TensorFlow
+
+This file is to be replaced by [this README.md](
+https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/mesh_tensorflow/README.md)
---
+++ oss_scripts/oss_release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -v  # print commands as they're executed
+set -e  # fail and exit on any command erroring
+
+GIT_COMMIT_ID=${1:-""}
+[[ -z $GIT_COMMIT_ID ]] && echo "Must provide a commit" && exit 1
+
+TMP_DIR=$(mktemp -d)
+pushd $TMP_DIR
+
+echo "Cloning tensorflow/mesh-tensorflow and checking out commit $GIT_COMMIT_ID"
+git clone https://github.com/tensorflow/mesh-tensorflow.git
+cd datasets
+git checkout $GIT_COMMIT_ID
+
+pip install wheel twine pyopenssl
+
+# Build the distribution
+echo "Building distribution"
+python setup.py sdist
+python setup.py bdist_wheel --universal
+
+# Publish to PyPI
+read -p "Publish? (y/n) " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Publishing to PyPI"
+  twine upload dist/*
+else
+  echo "Skipping upload"
+  exit 1
+fi
+
+# Cleanup
+rm -rf build/ dist/ mesh-tensorflow.egg-info/
+popd
+rm -rf $TMP_DIR
