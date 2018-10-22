a759cfb4bc946b455f06d4ef9377a937bd0dfb93
Dustin Tran <trandustin@google.com>
2018-10-22 11:16:15 -0700

    Attempt to fix Travis build errors for Python 3.
    PiperOrigin-RevId: 218199780

--- oss_scripts/oss_pip_install.sh
+++ oss_scripts/oss_pip_install.sh
@@ -14,6 +14,10 @@ fi
 
 # First ensure that the base dependencies are sufficient for a full import
 pip install -q -e .
-python -c "import mesh_tensorflow as mtf"
 
+# Then install the test dependencies
 pip install -q -e .[tests]
+
+# Make sure we have the latest version of numpy - avoid problems we were
+# seeing with Python 3
+pip install -q -U numpy
