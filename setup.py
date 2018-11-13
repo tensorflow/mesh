2b9e66c124b0d62f177a3875220714f4324ec0c0
Dustin Tran <trandustin@google.com>
2018-11-12 20:31:27 -0800

    Set up release to mesh-tensorflow 0.0.4.
    PiperOrigin-RevId: 221204704

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='mesh-tensorflow',
-    version='0.0.3',
+    version='0.0.4',
     description='Mesh TensorFlow',
     author='Google Inc.',
     author_email='no-reply@google.com',
