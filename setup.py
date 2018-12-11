12743d80f0912d9e52bdaa14d4e4260e9e7c07bf
Dustin Tran <trandustin@google.com>
2018-12-10 16:00:03 -0800

    Bump MTF version number.
    PiperOrigin-RevId: 224900496

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='mesh-tensorflow',
-    version='0.0.4',
+    version='0.0.5',
     description='Mesh TensorFlow',
     author='Google Inc.',
     author_email='no-reply@google.com',
