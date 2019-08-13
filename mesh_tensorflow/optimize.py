e55ae81101a05cb12db8d02d8161abd52ab129ce
Mesh TensorFlow Team <no-reply@google.com>
2019-08-13 00:18:43 -0700

    add adam optimizer.
    PiperOrigin-RevId: 263082321

--- mesh_tensorflow/optimize.py
+++ mesh_tensorflow/optimize.py
@@ -20,6 +20,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import re
 import gin
 from mesh_tensorflow import ops_with_redefined_builtins as mtf
 import tensorflow as tf
@@ -91,6 +92,80 @@ class SgdOptimizer(Optimizer):
     #  for the case of bfloat16 activations, so as to avoid repeatedly rounding
     #  the slice value, which results in poor quality.
     return [mtf.assign_sub(var, grad * self.lr)]
+
+
+@gin.configurable
+class AdamWeightDecayOptimizer(Optimizer):
+  """A basic Adam optimizer that includes "correct" L2 weight decay."""
+
+  def __init__(self,
+               learning_rate,
+               weight_decay_rate=0.0,
+               beta_1=0.9,
+               beta_2=0.999,
+               epsilon=1e-6,
+               exclude_from_weight_decay=None):
+    """Constructs a AdamWeightDecayOptimizer."""
+
+    self.learning_rate = learning_rate
+    self.weight_decay_rate = weight_decay_rate
+    self.beta_1 = beta_1
+    self.beta_2 = beta_2
+    self.epsilon = epsilon
+    self.exclude_from_weight_decay = exclude_from_weight_decay
+
+  def apply_grad(self, grad, var):
+    """See base class."""
+    if grad is None:
+      tf.logging.warning("Gradient is None for variable %s" % var.name)
+      return []
+    grad = mtf.to_float(grad)
+
+    assignments = []
+
+    m = mtf.get_variable(
+        var.mesh, var.name + "/adam_m", var.shape,
+        initializer=tf.zeros_initializer(), trainable=False)
+
+    v = mtf.get_variable(
+        var.mesh, var.name + "/adam_v", var.shape,
+        initializer=tf.zeros_initializer(), trainable=False)
+
+    # Standard Adam update.
+    next_m = self.beta_1 * m + (1.0 - self.beta_1) * grad
+    next_v = self.beta_2 * v + (1.0 - self.beta_2) * mtf.square(grad)
+
+    update = next_m / (mtf.sqrt(next_v) + self.epsilon)
+
+    # Just adding the square of the weights to the loss function is *not*
+    # the correct way of using L2 regularization/weight decay with Adam,
+    # since that will interact with the m and v parameters in strange ways.
+    #
+    # Instead we want ot decay the weights in a manner that doesn't interact
+    # with the m/v parameters. This is equivalent to adding the square
+    # of the weights to the loss with plain (non-momentum) SGD.
+    if self._do_use_weight_decay(var.name):
+      update += self.weight_decay_rate * var
+
+    update_with_lr = self.learning_rate * update
+
+    var_update = mtf.assign_sub(var, update_with_lr)
+
+    assignments.extend(
+        [var_update,
+         mtf.assign(m, next_m),
+         mtf.assign(v, next_v)])
+    return assignments
+
+  def _do_use_weight_decay(self, param_name):
+    """Whether to use L2 weight decay for `param_name`."""
+    if not self.weight_decay_rate:
+      return False
+    if self.exclude_from_weight_decay:
+      for r in self.exclude_from_weight_decay:
+        if re.search(r, param_name) is not None:
+          return False
+    return True
 
 
 @gin.configurable
