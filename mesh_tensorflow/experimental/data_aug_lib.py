5102b52dce66e4c5d32475fae5ca51114239e21e
Adam Roberts <adarob@google.com>
2019-09-17 11:20:56 -0700

    Remove google_type_annotations import in order to fix the build.
    PiperOrigin-RevId: 269612522

--- mesh_tensorflow/experimental/data_aug_lib.py
+++ mesh_tensorflow/experimental/data_aug_lib.py
@@ -20,7 +20,6 @@ A set of data augmentation functions.
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 
