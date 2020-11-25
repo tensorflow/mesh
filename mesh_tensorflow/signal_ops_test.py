69b2d76a7c2eb4f6493f04b95296874bafe63246
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-25 17:03:35 +0100

    corrected shaping for signal ops tests

--- mesh_tensorflow/signal_ops_test.py
+++ mesh_tensorflow/signal_ops_test.py
@@ -15,7 +15,10 @@ class FFTTest(tf.test.TestCase):
     cols_dim = mtf.Dimension("cols", volume_size)
     self.shape = [batch_dim, slices_dim, rows_dim, cols_dim,]
     volume_shape = [d.size for d in self.shape]
-    self.volume = tf.random.normal(volume_shape)
+    self.volume = tf.complex(
+        tf.random.normal(volume_shape),
+        tf.random.normal(volume_shape),
+    )
     self.volume_mesh = mtf.import_tf_tensor(self.mesh, self.volume, shape=self.shape)
 
 
@@ -23,30 +26,40 @@ class FFTTest(tf.test.TestCase):
     outputs = fft3d(self.volume_mesh, freq_dims=self.shape[1:4])
     assert len(outputs.shape) == 4
     assert outputs.dtype == tf.complex64
-    assert outputs.shape == self.shape
+    # assert outputs.shape == mtf.Shape(self.shape)
+    assert [d.size for d in outputs.shape] == [d.size for d in self.shape]
     mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(
       shape=[], layout={}, devices=[""])
     lowering = mtf.Lowering(self.graph, {self.mesh: mesh_impl})
     outputs_tf = lowering.export_to_tf_tensor(outputs)
-    tf_tester = tf.test.TestCase()
     expected_outputs = tf.signal.fft3d(self.volume)
-    tf_tester.assertAllEqual(
+    expected_outputs = tf.transpose(expected_outputs, perm=[0, 2, 3, 1])
+    self.assertAllClose(
       outputs_tf,
       expected_outputs,
+      rtol=1e-4,
+      atol=1e-4,
     )
 
   def testIfft3d(self):
-    outputs = ifft3d(self.volume_mesh, dims=self.shape[1:4])
+    outputs = ifft3d(
+        self.volume_mesh,
+        # ordering is not the same for ifft3d
+        dims=[self.shape[3], self.shape[1], self.shape[2]],
+    )
     assert len(outputs.shape) == 4
     assert outputs.dtype == tf.complex64
-    assert outputs.shape == self.shape
+    # assert outputs.shape == mtf.Shape(self.shape)
+    assert [d.size for d in outputs.shape] == [d.size for d in self.shape]
     mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(
       shape=[], layout={}, devices=[""])
     lowering = mtf.Lowering(self.graph, {self.mesh: mesh_impl})
     outputs_tf = lowering.export_to_tf_tensor(outputs)
-    tf_tester = tf.test.TestCase()
     expected_outputs = tf.signal.ifft3d(self.volume)
-    tf_tester.assertAllEqual(
+    expected_outputs = tf.transpose(expected_outputs, perm=[0, 3, 1, 2])
+    self.assertAllClose(
       outputs_tf,
       expected_outputs,
+      rtol=1e-4,
+      atol=1e-4,
     )
