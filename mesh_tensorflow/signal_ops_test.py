0e2760d068c4f41dedc98a66010c698d89fc732c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-25 16:30:30 +0100

    added tests for signal ops

---
+++ mesh_tensorflow/signal_ops_test.py
@@ -0,0 +1,53 @@
+import mesh_tensorflow as mtf
+from mesh_tensorflow.signal_ops import fft3d, ifft3d
+import tensorflow as tf
+
+
+class FFTTest(tf.test.TestCase):
+  def setUp(self):
+    super(FFTTest, self).setUp()
+    self.graph = mtf.Graph()
+    self.mesh = mtf.Mesh(self.graph, "my_mesh")
+    volume_size = 32
+    batch_dim = mtf.Dimension("batch", 1)
+    cols_dim = mtf.Dimension("cols", volume_size)
+    volume_channels_dim = mtf.Dimension('channels', 1)
+    slices_dim = mtf.Dimension("slices", volume_size)
+    rows_dim = mtf.Dimension("rows", volume_size)
+    self.shape = [batch_dim, slices_dim, rows_dim, cols_dim, volume_channels_dim]
+    volume_shape = [d.size for d in self.shape]
+    self.volume = tf.random.normal(volume_shape)
+    self.volume_mesh = mtf.import_tf_tensor(self.mesh, self.volume, shape=self.shape)
+
+
+  def testFft3d(self):
+    outputs = fft3d(self.volume_mesh, freq_dims=self.shape[1:4])
+    assert len(outputs.shape) == 4
+    assert outputs.dtype == tf.complex64
+    assert outputs.shape == self.shape
+    mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(
+      shape=[], layout={}, devices=[""])
+    lowering = mtf.Lowering(self.graph, {self.mesh: mesh_impl})
+    outputs_tf = lowering.export_to_tf_tensor(outputs)
+    tf_tester = tf.test.TestCase()
+    expected_outputs = tf.signal.fft3d(self.volume)
+    tf_tester.assertAllEqual(
+      outputs_tf,
+      expected_outputs,
+    )
+
+  def testIfft3d(self):
+    outputs = ifft3d(self.volume_mesh, dims=self.shape[1:4])
+    assert len(outputs.shape) == 4
+    assert outputs.dtype == tf.complex64
+    assert outputs.shape == self.shape
+    mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(
+      shape=[], layout={}, devices=[""])
+    lowering = mtf.Lowering(self.graph, {self.mesh: mesh_impl})
+    outputs_tf = lowering.export_to_tf_tensor(outputs)
+    tf_tester = tf.test.TestCase()
+    expected_outputs = tf.signal.ifft3d(self.volume)
+    tf_tester.assertAllEqual(
+      outputs_tf,
+      expected_outputs,
+    )
